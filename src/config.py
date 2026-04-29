e82d0082b673006ee7dce31ebbe4085b33963b3f
BTExpress1 <34968058+BTExpress1@users.noreply.github.com>
2026-04-28 22:08:53 -0700

    RunPod files ready

--- src/config.py
+++ src/config.py
@@ -20,7 +20,11 @@ CLASS_NAMES = ["akiec", "bcc", "bkl", "df", "mel", "nv", "vasc"]
 
 RANDOM_SEED = 123
 
-SAMPLE_FRAC = 0.30    # use 30% dataset
-EPOCHS = 10
+
+
+SAMPLE_FRAC = 1.0
+EPOCHS = 20
+UNFREEZE_EPOCH = 7
+BATCH_SIZE = 64
 LEARNING_RATE = 1e-4
 
